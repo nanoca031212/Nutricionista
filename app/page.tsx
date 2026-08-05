5077678ebd4be86db36b1facaec9ef4cf693e527
nanoca031212 <lucasnatanael542@gmail.com>
2026-08-05 12:46:29 -0300

    penos sessão

--- app/page.tsx
+++ app/page.tsx
@@ -20,11 +20,9 @@ export default function Home() {
         <ProblemSection />
 
         <Benefits />
-        <MethodologySection />
-        <Methodology />
 
         <About />
-        <Testimonials />
+
         <FAQ />
       </main>
       <Footer />
